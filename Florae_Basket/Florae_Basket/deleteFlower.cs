bb5c4741f4606d581833eee6f64d7c3d71432087
Kadwalt <Kadwalt@siue.edu>
2018-03-17 14:44:44 -0500

    added methods that outline how the delete flower class will work.

--- Florae_Basket/Florae_Basket/deleteFlower.cs
+++ Florae_Basket/Florae_Basket/deleteFlower.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Florae_Basket
 {
     class deleteFlower
     {
 
+        // get the current flowers id # in the database
+        int flowerID()
+        {
+            int id = Flower.getID();
+            return id;
+        }
+
+        // remove the flower from the database
+        void removeFlower(int id)
+        {
+            // database connection variables and info
+
+
+
+            // try to open the database and delete
+            try
+            {
+                // database connection info
+
+                // successful deletion message
+                MessageBox.Show("Flower deleted.");
+            }
+            // if error is thrown, return false
+            catch(Exception ex)
+            {
+                // error in deletion message
+                MessageBox.Show("Error when deleting. Flower not deleted.");
+                return;
+            }
+        }
+
+        void main()
+        {
+            int id = flowerID();
+            removeFlower(id);
+            return;
+        }
     }
 }
