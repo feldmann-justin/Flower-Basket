e127fa7454b8a22a3abcf4688bf41764facafd63
bradsmi <bradsmi@siue.edu>
2018-03-17 19:41:05 -0500

    Added more to Word Search.

--- Florae_Basket/Florae_Basket/Program.cs
+++ Florae_Basket/Florae_Basket/Program.cs
@@ -18,9 +18,12 @@ namespace Florae_Basket
             Application.SetCompatibleTextRenderingDefault(false);
             Test_WS_GUI test = new Test_WS_GUI();
             Application.Run(test);
-            if (test.egn_name != null || test.latin_word != null || test.botanical != null)
+            if ((test.egn_name   != null && test.egn_name   != "") || 
+                (test.latin_word != null && test.latin_word != "") || 
+                (test.botanical  != null && test.botanical  != ""))
             {
                 Word_Search word = new Word_Search(test.egn_name, test.latin_word, test.botanical);
+                word.Search();
             }
         }
     }
--- Florae_Basket/Florae_Basket/Word_Search.cs
+++ Florae_Basket/Florae_Basket/Word_Search.cs
@@ -8,19 +8,24 @@ namespace Florae_Basket
 {
     class Word_Search
     {
-        struct Candidate
+        //contains the string of the candidate, as well as the database key
+        //and the score calculated from the LCS method.
+        public struct Candidate
         {
-            string contents;
-            int id;
-            int score;
+            public string contents;
+            public int id;
+            public double score;
         };
 
-        private int[] results = new int[3];
+        private int[] results = new int[3];              //array containing the database id of the 3 best results.
+        private Candidate[] best_names = new Candidate[3];
+        private Candidate[] best_latin = new Candidate[3];
+        private Candidate[] best_botan = new Candidate[3];
         private string name;
         private string latin;
         private string botan;
-        private LinkedList<Candidate> possible_names;
-        private LinkedList<Candidate> possible_latin;
+        private LinkedList<Candidate> possible_names;    //Linked list containing the English names pulled from the database.
+        private LinkedList<Candidate> possible_latin;    
         private LinkedList<Candidate> possible_botan;
 
         public Word_Search(string enl_name, string latin_name, string botan_name)
@@ -50,40 +55,91 @@ namespace Florae_Basket
             return results;
         }
 
+        //Longest Common SubString
+        //Calculates the match score for two strings.
+        //This score represents the number of characters each string has in common.
         private int LCS(string entry, string cand, int x, int y)
         {
+            //base case
             if (x == 0 || y == 0)
             {
                 return 0;
             }
+            //when a letter matches add one to score
             else if (entry[x] == cand[y])
             {
                 return Math.Max(1 + LCS(entry, cand, x - 1, y - 1), Math.Max(LCS(entry, cand, x - 1, y), LCS(entry, cand, x, y - 1)));
             }
+            //when letter does not match evaluate 
             else
             {
                 return Math.Max(LCS(entry, cand, x - 1, y), LCS(entry, cand, x, y - 1));
             }
         }
 
-        private void Fetch_names(string name)
+        //Fetch english names from the Database using just first letter of the search entry
+        //Stores results into linked list that is passed by reference
+        private void Fetch_names(string name, ref LinkedList<Candidate> list)
         {
             
         }
 
-        private void Fetch_latin(string latin)
+        //Fetch latin names from the Database using just first letter of the search entry
+        //Stores results into linked list that is passed by reference
+        private void Fetch_latin(string latin, ref LinkedList<Candidate> list)
         {
 
         }
 
-        private void Fetch_botan(string botan)
+        //Fetch botanical family names from the Database using just first letter of the search entry
+        //Stores results into linked list that is passed by reference
+        private void Fetch_botan(string botan, ref LinkedList<Candidate> list)
         {
 
         }
 
+        //executes the word search and populates the results array
         public void Search()
         {
-
+            Candidate temp;
+            possible_names.Clear();
+            //Makes sure name has a value
+            if (name != null && name != "")
+            {
+                Fetch_names(name, ref possible_names);
+                //Loops through pulled list
+                for (int i = 0; i < possible_names.Count; i++)
+                {
+                    //stores current candidate into for testing
+                    temp = possible_names.ElementAt(i);
+                    //runs LCS on current candidate
+                    temp.score = LCS(name, temp.contents, name.Length, temp.contents.Length);
+                    //Compares score with current best score
+                    if (temp.score > best_names[0].score)
+                    {
+                        best_names[2] = best_names[1];
+                        best_names[1] = best_names[0];
+                        best_names[0] = temp;
+                    }
+                    //compares with second highest
+                    else if (temp.score > best_names[1].score)
+                    {
+                        best_names[2] = best_names[1];
+                        best_names[1] = temp;
+                    }
+                    //compares with third highest
+                    else if (temp.score > best_names[2].score)
+                    {
+                        best_names[2] = temp;
+                    }
+                    //reseting temp value
+                    temp.score = 0.0;
+                    temp.id = 0;
+                    temp.contents = "";
+                }
+            }
+            //clearing list for next word to be run
+            possible_names.Clear();
         }
     }
 }
