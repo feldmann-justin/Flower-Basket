f08a94fba2008b53771dfc37c946e15df671e946
jufeldm <jufeldm@siue.edu>
2018-03-18 13:48:36 -0500

    Simply changed the title of the GUI box to "Add Flower".

--- Florae_Basket/Florae_Basket/AddFlowerGUI.Designer.cs
+++ Florae_Basket/Florae_Basket/AddFlowerGUI.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+			this.SuspendLayout();
+			// 
+			// AddFlowerGUI
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(282, 253);
+			this.Name = "AddFlowerGUI";
+			this.Text = "Add Flower";
+			this.ResumeLayout(false);
+
         }
 
         #endregion
