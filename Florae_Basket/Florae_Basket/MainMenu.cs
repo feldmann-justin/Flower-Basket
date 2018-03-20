92f9f952d82ca494ec85da66bf0fe9ebf035c851
jufeldm <jufeldm@siue.edu>
2018-03-20 18:31:45 -0500

    (In progress) Created a main menu GUI which contains buttons to launch both the Add Flower and Search Flower GUIs from said main menu.

---
+++ Florae_Basket/Florae_Basket/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Florae_Basket
+{
+	public partial class MainMenu : Form
+	{
+		public MainMenu()
+		{
+			InitializeComponent();
+		}
+
+		private void addFlowerMainMenuBtn_Click(object sender, EventArgs e)
+		{
+			Application.Run(new AddFlowerGUI());
+		}
+
+		private void searchFlowerMainMenuBtn_Click(object sender, EventArgs e)
+		{
+			Test_WS_GUI test = new Test_WS_GUI();
+			Application.Run(test);
+			if ((test.egn_name != null && test.egn_name != "") ||
+				(test.latin_word != null && test.latin_word != "") ||
+				(test.botanical != null && test.botanical != ""))
+			{
+				Word_Search word = new Word_Search(test.egn_name, test.latin_word, test.botanical);
+				word.Search();
+			}
+		}
+	}
+}
--- Florae_Basket/Florae_Basket/Program.cs
+++ Florae_Basket/Florae_Basket/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,15 +16,18 @@ namespace Florae_Basket
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Test_WS_GUI test = new Test_WS_GUI();
-            Application.Run(test);
-            if ((test.egn_name   != null && test.egn_name   != "") || 
-                (test.latin_word != null && test.latin_word != "") || 
-                (test.botanical  != null && test.botanical  != ""))
-            {
-                Word_Search word = new Word_Search(test.egn_name, test.latin_word, test.botanical);
-                word.Search();
-            }
-        }
+
+			// moved this code to 'searchFlowerMainMenuBtn' event handler in 'MainMenu.cs'
+
+			//Test_WS_GUI test = new Test_WS_GUI();
+			//Application.Run(test);
+			//if ((test.egn_name != null && test.egn_name != "") ||
+			//	(test.latin_word != null && test.latin_word != "") ||
+			//	(test.botanical != null && test.botanical != ""))
+			//{
+			//	Word_Search word = new Word_Search(test.egn_name, test.latin_word, test.botanical);
+			//	word.Search();
+			//}
+		}
     }
 }
