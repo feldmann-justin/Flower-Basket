70038732ea2844314d21f5e37186f03b5f805e2c
jufeldm <jufeldm@siue.edu>
2018-04-05 01:11:42 -0500

    Started creation of ChangeFlowerCtlr and ChangeFlowerGUI. Overall ChangeFlowerGUI button/text box layout done. TODO: Add functionality to event handlers, verifyFlower() in ChangeFlowerCtlr.

---
+++ Florae_Basket/Florae_Basket/ChangeFlowerCtlr.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name: Justin Feldmann
+ * Class: CS 325-002
+ * Semester: Spring 2018
+ * Filename: ChangeFlowerCtlr.cs
+ * Professor: Dr. Gary Mayer
+ * Last Edited Date/Time: March 18, 2018 12:09 AM
+ * 
+ * Description: This class contains the necessary constructors, class attributes,
+ * getters and setters for a ChangeFlower controller object.
+ */
+
+namespace Florae_Basket
+{
+	class ChangeFlowerCtlr
+	{
+
+		// member functions
+
+
+		// This version of verifyFlower() will verify that the flower entry
+		// passed as an argument does not exist within the database, rather than 
+		// comparing with the contents of a pseudo-database, or a dummy Flower instantiation,
+		// like what was done for Iteration I.
+		public int verifyFlower(Flower customFlower)
+		{
+
+			return 0;
+
+		}
+
+		public ChangeFlowerCtlr()
+		{
+
+
+
+		}
+
+	}
+}
