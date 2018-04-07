8ccc1b92e937af34a2f9086b55fc31a79e1ddadf
bradsmi <bradsmi@siue.edu>
2018-04-07 14:06:56 -0500

    Added a unit test project for everyone to make their unit tests in.

---
+++ Florae_Basket/Florae_Basket.Tests/FloraeBasketTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Florae_Basket;
+
+namespace Florae_Basket.Tests
+{
+    [TestClass]
+    public class FloraeBasketTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
