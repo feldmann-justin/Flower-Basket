9e77e5805d5b5a91c0ab4b404c798cc4aad63ace
Dipak Subramaniam <dsubram@siue.edu>
2018-04-11 23:58:28 -0500

    Final Local Commit for Iteration II

--- Florae_Basket/Florae_Basket/ImageSearch.Designer.cs
+++ Florae_Basket/Florae_Basket/ImageSearch.Designer.cs
@@ -141,9 +141,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(432, 294);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.Size = new System.Drawing.Size(80, 13);
             this.label1.TabIndex = 10;
-            this.label1.Text = "Matches:";
+            this.label1.Text = "Match Indexes:";
             // 
             // resultsLabel
             // 
