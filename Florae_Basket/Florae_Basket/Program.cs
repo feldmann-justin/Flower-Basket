6d9aa0964a8fa0feb8831013fc907e28fb0984e4
bradsmi <bradsmi@siue.edu>
2018-04-22 01:16:24 -0500

    Testing database stuff

--- Florae_Basket/Florae_Basket/Program.cs
+++ Florae_Basket/Florae_Basket/Program.cs
@@ -18,16 +18,10 @@ namespace Florae_Basket
 
 			// giving 'userAcctType' a value of 1 (Student) just to test
 			// until integration with Dipak's Login code
-			int userAcctType = 1;
+			int userAcctType = 3;
 
             Database_Manager db = new Database_Manager();
             //FOR TESTING ONLY, FLOWERS SHOULD NOT BE INSERTED HERE
-            db.InsertFlower("violet", "violat", "vio fam", "vio note", "flower.jpg");
-            db.InsertFlower("rose", "roselat", "ros fam", "ros  note", "flower.jpg");
-            db.InsertFlower("tulip", "tulilat", "tul fam", "tul note", "flower.jpg");
-            db.DeleteFlower(1);
-            db.DeleteFlower(2);
-            db.DeleteFlower(3);
             //FOR TESTING ONLY, FLOWERS SHOULD NOT BE INSERTED HERE
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
